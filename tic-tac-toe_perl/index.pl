1dda44f0e7c6be7269ed96d76fd22e7655b6f63f
JP McDowell <jp.mcdowell@gmail.com>
2022-10-14 23:06:51 -0700

    Commit me

---
+++ tic-tac-toe_perl/index.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+use 5.010;
+
+# Numeric    String         Meaning
+# ==            eq           equal
+# !=            ne           not equal
+# <             lt           less than
+# >             gt           greater than
+# <=            le           less than or equal
+# >=            ge           greater than or equal
+
+print "Hello World\n";
+

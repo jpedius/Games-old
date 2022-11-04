2cc9bca39f5a9a61231cd1dc0c7d1698bd8703cc
JP McDowell <jp.mcdowell@gmail.com>
2022-11-03 23:02:04 -0700

    Commit me

--- tic-tac-toe_perl/index.pl
+++ tic-tac-toe_perl/index.pl
@@ -1,18 +1,63 @@
+#!/usr/bin/env perl
+
 use strict;
 use warnings;
 use 5.010;
 
-# Numeric    String         Meaning
-# ==            eq           equal
-# !=            ne           not equal
-# <             lt           less than
-# >             gt           greater than
-# <=            le           less than or equal
-# >=            ge           greater than or equal
+use Scalar::Util qw(looks_like_number);
 
-print "Hello World\n";
+my @a  = ( " ", " ", " ", " ", " ", " ", " ", " ", " " );
 
-for (my $i=0; $i <= 9; $i++) {
-   print "$i\n";
+my @b = (
+	"                      \n",
+	"          |     |     \n",
+	"       " . $a[0] . "  |  " . $a[1] . "  |  " . $a[2] . "      \n",
+	"          |     |     \n",
+	"     -----+-----+-----\n",
+	"          |     |     \n",
+        "       " . $a[3] . "  |  " . $a[4] . "  |  " . $a[5] . "      \n",
+        "          |     |     \n",		
+	"     -----+-----+-----\n",
+        "          |     |     \n",
+        "       " . $a[6] . "  |  " . $a[7] . "  |  " . $a[8] . "      \n",
+        "          |     |     \n",
+	"                      \n",
+	"     Player: ",
+);
+
+my @c = ( "X", "O" );
+
+while (1) {
+
+	foreach my $d (@b) { print $d; }
+
+	chomp (my $e = <>);
+	my $f = length $e;
+	print "\n";
+
+	if ($f == 1) {
+		my $isnum = looks_like_number( $e );
+		# print "Print " . $isnum;
+		if ($isnum) {
+			my $g = int($e);
+			if (($g >= 0) and ($g <= 8)) {
+				$a[$g] = "X";
+				print $a[$g] .  " my measurement so far";
+				for (my $h=0; $h<9; $h++) {
+					print $a[$h] . " ";
+				}
+				if ($g == 8) {
+					last;
+				}
+
+			} else {
+				next;
+			}
+		} else {
+			next;
+		}
+	} else {
+		next;
+	}	
 }
 
